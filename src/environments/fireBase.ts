c3d7916dcf3fcf30b5f66d9da2953f79590d0404
evanschaba <evanschaba@gmail.com>
2021-01-14 02:19:07 +0200

    :wrench:test-fb-config

---
+++ src/environments/fireBase.ts
@@ -0,0 +1,11 @@
+export const firebaseConfig = {
+	apiKey: 'AIzaSyA0ufLLG1Pleb7vtBTAUOeBOVv2BFfbw4c',
+	authDomain: 'storello-5f13e.firebaseapp.com',
+	databaseURL:
+		'https://storello-5f13e-default-rtdb.europe-west1.firebasedatabase.app',
+	projectId: 'storello-5f13e',
+	storageBucket: 'storello-5f13e.appspot.com',
+	messagingSenderId: '713575306048',
+	appId: '1:713575306048:web:cfb876ba9e82c36c1120a5',
+	measurementId: 'G-9HJJDJ3PBB',
+};
